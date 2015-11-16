4ea2ad9f7bfe9bdb931caa7889a8982b084cd1ff
Matthew Wolff <mwolff3@lsu.edu>
2015-11-15 23:25:41 -0600

    removed external buffer

--- edge/edge.js
+++ edge/edge.js
@@ -1,8 +1,7 @@
 var player;
 
 function init() {
-  player = new Tone.Player().toMaster();
+  player = new Tone.Player("https://mwolff3.github.io/InfiniteSmashMouth/edge/edge.mp3").toMaster();
   player.loop = true;
-  player.buffer = new Tone.Buffer("https://mwolff3.github.io/InfiniteSmashMouth/edge/edge.mp3");
   Tone.Buffer.onload = function() {player.start()};
 }
