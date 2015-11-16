697bfa0bda523f0f51e90ca50851b2bdebe3dec7
Matthew Wolff <mwolff3@lsu.edu>
2015-11-15 23:22:22 -0600

    let's just do what tone says

--- edge/edge.js
+++ edge/edge.js
@@ -1,10 +1,9 @@
 var player;
 
 function init() {
-  player = new Tone.Player();
+  player = new Tone.Player().toMaster();
   player.loop = true;
-  player.buffer = new Tone.AudioBuffer("https://mwolff3.github.io/InfiniteSmashMouth/edge/edge.mp3",function() {
-    player.toMaster();
-    player.start();
+  player.buffer = new Tone.Buffer("https://mwolff3.github.io/InfiniteSmashMouth/edge/edge.mp3");
+  Tone.Buffer.onload = function() {player.start()};
   });
 }
