0a62327233978047b586c84712f293ee62aced52
Matthew Wolff <mwolff3@lsu.edu>
2015-11-15 23:21:03 -0600

    Update edge.js

--- edge/edge.js
+++ edge/edge.js
@@ -1,7 +1,7 @@
 var player;
 
 function init() {
-  player = new Tone.Player().toMaster();
+  player = new Tone.Player();
   player.loop = true;
   player.buffer = new Tone.AudioBuffer("https://mwolff3.github.io/InfiniteSmashMouth/edge/edge.mp3",function() {
     player.toMaster();
