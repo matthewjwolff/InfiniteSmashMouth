765e5f296938f6c7e885171d7de346b694f2a7f4
Matthew Wolff <mwolff3@lsu.edu>
2015-11-15 23:15:31 -0600

    fixed edge link

--- edge/edge.js
+++ edge/edge.js
@@ -1,5 +1,5 @@
 function init() {
   var player = new Tone.Player().toMaster();
   player.loop = true;
-  player.buffer = new Tone.Buffer("https://mwolff3.github.io/edge/edge.mp3",function() {player.start();});
+  player.buffer = new Tone.Buffer("https://mwolff3.github.io/InfiniteSmashMouth/edge/edge.mp3",function() {player.start();});
 }
