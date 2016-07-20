092a7d8250aa8255e75c84d4679cf61ab90f6a28
Matthew Wolff <matthew.wolff@ge.com>
2016-07-20 14:36:09 -0500

    added anime smash mouth

---
+++ anime.js
@@ -0,0 +1,17 @@
+(() => {
+  var context = new AudioContext()
+  var source = context.createBufferSource()
+  var request = new XMLHttpRequest()
+  request.open('GET', './allstar.mp3', true)
+  request.responseType = 'arraybuffer'
+  request.onload = () => {
+    var audioData = request.response
+    context.decodeAudioData(audioData, (buffer) => {
+      source.buffer = buffer
+      source.connect(context.destination)
+      source.loop=true
+      source.detune.value = 1000
+      source.start()
+    })
+  }
+})()
