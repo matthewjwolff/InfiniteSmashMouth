35256be804601cbac4352e80656cd0118506c3a7
Matthew Wolff <matthewjwolff@gmail.com>
2018-03-10 17:24:05 -0600

    moved dead link

--- chunk.js
+++ chunk.js
@@ -1,5 +1,7 @@
 function init() {
-	buffer = new Tone.Buffer("https://dl.dropboxusercontent.com/u/37873577/chunk.mp3", function(){
+	buffer = new 
+Tone.Buffer("https://matthewjwolff.github.io/InfiniteSmashMouth/chunk.mp3", 
+function(){
 		meme();
 	});
 }
