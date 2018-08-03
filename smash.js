c43bf51f67efe3abf756e9bae67a6f83b5efc045
Matthew Wolff <matthewjwolff@gmail.com>
2018-08-03 14:01:56 -0500

    Update smash.js

--- smash.js
+++ smash.js
@@ -1,5 +1,5 @@
 function init() {	
-	buffer = new Tone.Buffer("https://matthewjwolff.github.io/InfiniteSmashMouth/allstar.mp3", function(){
+	buffer = new Tone.Buffer("InfiniteSmashMouth/allstar.mp3", function(){
 		meme();
 	});
 }
